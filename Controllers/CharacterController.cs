e22aeec0574e81a90f84c8b3604892f126c1dcba
AlbertoGonzalezSaenz <alberto.gonzalez@kinandcarta.com>
2022-05-25 16:09:12 -0400

    Added Character and RpgClass Models and Character controller

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg_2.Controllers
+{
+    [ApiController] // Adds api specific features 
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character { Name = "Sam"}
+        };
+
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet]
+        public ActionResult<Character> GetSingle()
+        {
+            return Ok(characters[0]);
+        }
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,12 @@
+using System.Text.Json.Serialization;
+
+namespace dotnet_rpg_2.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight,
+        Mage,
+        Cleric
+    }
+}
